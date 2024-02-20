25ff96f67f502f8f245754cdd4dcc6b67819092e
david_ohayon <dav21189@gmail.com>
2024-02-21 01:18:28 +0200

    pull from my gitlab

---
+++ Barrier.h
@@ -0,0 +1,22 @@
+// vi2da
+
+#ifndef BARRIER_H
+#define BARRIER_H
+#include <pthread.h>
+
+// a multiple use barrier
+
+class Barrier {
+public:
+    Barrier(int numThreads);
+    ~Barrier();
+    void barrier();
+
+private:
+    pthread_mutex_t mutex;
+    pthread_cond_t cv;
+    int count;
+    int numThreads;
+};
+
+#endif //BARRIER_H
